557f18b36b22b417b0810010c831e444669235c9
Shrek <100253910+Shrek069@users.noreply.github.com>
2022-06-30 20:12:50 +0530

    Update app.py
    Added notes (#)

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import random
 import time
 
+# Takes inputs
+
 delay = input("What is the delay between generating new numbers?: ")
 max = input("What is the max random number to generate?: ")
 
+
+# Does the actual thing
 time.sleep(2.5)
 print("Started Generating Numbers")
 
